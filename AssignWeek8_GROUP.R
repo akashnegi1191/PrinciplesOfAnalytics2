4374c267c796f05dafd39d948800d17cfc8236d2
Bhawana Rijhwani <bhawna.rjw@gmail.com>
2017-05-21 23:34:37 -0400

    Cleanup

--- AssignWeek8_GROUP.R
+++ AssignWeek8_GROUP.R
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-# CODE FOR CLEANUP - AKASH NEGI
-
-library(readr)
-dirty_data <- read_csv("~/Downloads/dirty_data.csv")  # SPECIFY LOCATION HERE
-
-#install.packages("stringr")
-library(stringr)
-
-# REMOVING ALL SPECIAL CHARACTERS USIGN REGEX
-dirty_data$Street = str_replace_all(dirty_data$Street, "[^a-zA-Z0-9]", " ")
-dirty_data$`Street 2` = str_replace_all(dirty_data$`Street 2`, "[^a-zA-Z0-9]", " ")
-
-# REMOVING ALL PUNCTUATIONS
-dirty_data$Street = str_replace_all(dirty_data$Street, "[[:punct:]]", " ")
-dirty_data$`Street 2` = str_replace_all(dirty_data$`Street 2`, "[[:punct:]]", " ")
-
-# REMOVING ALL WHITESPACES
-dirty_data$Street = trimws(dirty_data$Street, c("both"))
-dirty_data$`Street 2` = trimws(dirty_data$`Street 2`, c("both"))
-
-# CAPITALIZING EACH FIRST LETTER
-library(tools)
-dirty_data$Street = tolower(dirty_data$Street)
-dirty_data$`Street 2` = tolower(dirty_data$`Street 2`)
-dirty_data$Street = toTitleCase(dirty_data$Street)
-dirty_data$`Street 2` = toTitleCase(dirty_data$`Street 2`)
-
-# REPLACING ALL "Street" with "Str."
-dirty_data$Street = str_replace_all(dirty_data$Street, "Street", "Str.")
-dirty_data$Street = str_replace_all(dirty_data$Street, "Road", "Rd.")
-dirty_data$Street = str_replace_all(dirty_data$Street, "Avenue", "Ave.")
-
-dirty_data$`Street 2` = str_replace_all(dirty_data$`Street 2`, "Street", "Str.")
-dirty_data$`Street 2` = str_replace_all(dirty_data$`Street 2`, "Road", "Rd.")
-dirty_data$`Street 2` = str_replace_all(dirty_data$`Street 2`, "Avenue", "Ave.")
-
-################################################################################
-# CODE FOR CLEANUP - BHAWANA RIJHWANI
-dirty_data$Strange.HTML = NULL
-
-
-################################################################################
-# CODE FOR CLEANUP - VIJAYANAVEEN MURRAGUNDLA
-
-
-
-
-################################################################################
-# CODE FOR CLEANUP - APOORVA SURAT
-
-=======
 # CODE FOR CLEANUP - AKASH NEGI
 
 library(readr)
@@ -100,4 +48,3 @@ dirty_data[!duplicated(dirty_data$Street.2), ]
 ################################################################################
 # CODE FOR CLEANUP - APOORVA SURAT
 
->>>>>>> aee7b05282f1141dd6617acd586449c2326021aa
